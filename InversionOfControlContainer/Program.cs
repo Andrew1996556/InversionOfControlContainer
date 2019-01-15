5a2a9f7a63a42c677bfdb16b5e69345b03c2495c
Andrew Levchenko <andrew.projectone@gmail.com>
2019-01-15 19:15:43 +0200

    Добавьте файлы проекта.

---
+++ InversionOfControlContainer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InversionOfControlContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
